6415ba26ebe90d67b12be80b6faea648b12dca9f
Rani Saad <rsaad@live.ca>
2021-07-18 23:57:24 -0400

    First commit

---
+++ Develop/client/src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+
+import { setContext } from "@apollo/client/link/context";
+
+function App() {
+  const httpLink = createHttpLink({ uri: "/graphql" });
+
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem("id_token");
+
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route
+              render={() => (
+                <h1 className="display-2">This is the wrong page!</h1>
+              )}
+            />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
