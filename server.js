cc0e94ed665153bc6feb539b27103482a86ac72a
Rani Saad <rsaad@live.ca>
2021-07-19 18:16:36 -0400

    Installed missing packages

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require("path");
-const db = require("./config/connection");
+const db = require("./Develop/server/config/connection");
 
 const { ApolloServer } = require("apollo-server-express");
-const { typeDefs, resolvers } = require("./schemas/index");
-const { authMiddleware } = require("./utils/auth");
+const { typeDefs, resolvers } = require("./Develop/server/schemas/index");
+const { authMiddleware } = require("./Develop/server/utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
